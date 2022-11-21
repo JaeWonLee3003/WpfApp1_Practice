fb9e19333ce552adbdceaf41fd07106aaad416a6
Thinking^loper <96408793+JaeWonLee3003@users.noreply.github.com>
2022-11-21 16:19:57 +0900

    Plus + minus + Score box add and ttt board add

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -25,9 +25,31 @@ namespace WpfApp1
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void plus_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Hello World");
+            int ori = int.Parse(numbox.Text); // "0" string -> int 0
+            int target = ori + 1; // 1
+            numbox.Text = target.ToString();
+
+
+            //MessageBox.Show("Hello World");
+            //outputTb.Text = mytb.Text;
+            //outputTb.FontSize += 2;
+            //Grid.SetColumn(mytb, 0);
+            //mytb.Text = "Hello World";
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void minus_Click(object sender, RoutedEventArgs e)
+        {
+            int ori = int.Parse(numbox.Text); // "0" string -> int 0
+            int target = ori - 1; // 1
+            numbox.Text = target.ToString();
         }
     }
 }
